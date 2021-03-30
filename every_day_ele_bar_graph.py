7905bd5f695f3491ae6ba33c16e773e07bcb1da1
yudongyue <yudongyue@daqo.com>
2021-03-30 16:48:37 +0800

    添加给图片绘制文字实例

--- every_day_ele_bar_graph.py
+++ every_day_ele_bar_graph.py
@@ -16,14 +16,23 @@ day_ele_all.index = day_ele_all.index + 1
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot()
 xlist = [str(i) for i in day_ele_all.index]
-ax.bar(xlist, day_ele_all['总用电量'], color='orange')
-# plt.grid(axis='y')
+
 plt.xticks(rotation=-30)  # 斜着展示，角度从逆时针开始算。
 plt.title('每日用电量统计', fontsize=25, ha="center")
 plt.xlabel('时间（日）')
 plt.ylabel('用电量（KWh）')
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
+ax.bar(xlist, day_ele_all['总用电量'], color='orange')
+#  设置是否显示刻度
+ax.tick_params(bottom=False, left=False, labelsize="15.0")
+#   添加辅助线及颜色
+plt.grid(axis='y', color="white")
+# 设置是否显示边框及颜色
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_color("lightgray")
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_color("lightgray")
 plt.show()
 path = './/plot_folder_user//'
 plt.savefig(path + '每日用电量统计')
