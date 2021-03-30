f293529237ba350b560995dfef5ba56887cccfdb
yudongyue <yudongyue@daqo.com>
2021-03-30 16:51:11 +0800

    添加给图片绘制文字实例

--- every_day_ele_bar_graph.py
+++ every_day_ele_bar_graph.py
@@ -2,6 +2,7 @@
 # date: 2021/3/26
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.font_manager import FontProperties
 
 day_ele_all_a = {
     "总用电量": [
@@ -18,9 +19,10 @@ ax = fig.add_subplot()
 xlist = [str(i) for i in day_ele_all.index]
 
 plt.xticks(rotation=-30)  # 斜着展示，角度从逆时针开始算。
-plt.title('每日用电量统计', fontsize=25, ha="center")
-plt.xlabel('时间（日）')
-plt.ylabel('用电量（KWh）')
+font_microsoft = FontProperties(fname=r"C:\Windows\Fonts\msyhbd.ttc", size="17")
+plt.title('每日用电量统计', fontsize=25, ha="center", fontproperties=font_microsoft)
+plt.xlabel('时间（日）', size="15")
+plt.ylabel('用电量（KWh）', size="15")
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.bar(xlist, day_ele_all['总用电量'], color='orange')
