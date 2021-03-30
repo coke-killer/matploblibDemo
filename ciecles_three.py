d86b7c7bf4b74e134a729ac99a15622dc03adff2
yudongyue <yudongyue@daqo.com>
2021-03-30 16:38:05 +0800

    添加给图片绘制文字实例

--- ciecles_three.py
+++ ciecles_three.py
@@ -9,7 +9,8 @@ bk_png = cv2.imread("plot_folder_user/img_4.png")
 img_pil = Image.fromarray(bk_png)
 draw = ImageDraw.Draw(img_pil)
 # 设置需要显示的字体
-font = ImageFont.truetype("font/simsun.ttc", 25, ImageFont.LAYOUT_RAQM)
+# font = ImageFont.truetype("font/simsun.ttc", 25, ImageFont.LAYOUT_RAQM)
+font = ImageFont.truetype("font/msyhbd.ttc", 25, ImageFont.LAYOUT_RAQM)
 font2 = ImageFont.truetype("font/simsun.ttc", 45, ImageFont.LAYOUT_BASIC)
 # 绘制文字信息
 total_ele = 861123333
@@ -37,9 +38,9 @@ total_RMB__location_dict = {
     "9": (685, 212)
 }
 Ele_data = [['总用电量(KWh)', '日平均用电量（KWh）', '总电费（RMB）'], [total_ele, ele_per_day, total_RMB]]
-draw.text((145, 37), '总用电量(KWh)', font=font, fill=(166, 108, 90), fontweight='bold')
-draw.text((407, 37), '日平均用电量(KWh)', font=font, fill=(199, 188, 233))
-draw.text((722, 37), '总电费(RMB)', font=font, fill=(51, 132, 217))
+draw.text((135, 35), '总用电量(kW·h)', font=font, fill=(166, 108, 90), fontweight='bold')
+draw.text((392, 35), '日平均用电量(kW·h)', font=font, fill=(199, 188, 233))
+draw.text((713, 35), '总电费(RMB)', font=font, fill=(51, 132, 217))
 draw.text(total_ele_location_dict[str(len(str(Ele_data[1][0])))], str(Ele_data[1][0]), font=font2, fill=(0, 0, 255))
 draw.text(ele_per_day_location_dict[str(len(str(Ele_data[1][1])))], str(Ele_data[1][1]), font=font2, fill=(0, 0, 255))
 draw.text(total_RMB__location_dict[str(len(str(Ele_data[1][2])))], str(Ele_data[1][2]), font=font2, fill=(0, 0, 255))
@@ -48,5 +49,5 @@ bk_png = np.array(img_pil)
 cv2.imshow("add_text", bk_png)
 cv2.waitKey()
 #  保存图片
-cv2.imencode('.jpg', bk_png)[1].tofile("plot_folder_user/图片.png")
+cv2.imencode('.jpg', bk_png)[1].tofile("plot_folder_user/图片_xxx.png")
 # cv2.imwrite("plot_folder_user/图片.png", bk_png)
--- every_day_ele_bar_graph.py
+++ every_day_ele_bar_graph.py
@@ -1,2 +1,31 @@
 # __author__: "yudongyue"
 # date: 2021/3/26
+import matplotlib.pyplot as plt
+import pandas as pd
+
+day_ele_all_a = {
+    "总用电量": [
+        284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900,
+        284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900,
+        284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900,
+        284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900, 284721.335900,
+        284721.335900, 284721.335900, 284721.335900
+    ]}
+day_ele_all = pd.DataFrame(day_ele_all_a)
+day_ele_all.index = day_ele_all.index + 1
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot()
+xlist = [str(i) for i in day_ele_all.index]
+ax.bar(xlist, day_ele_all['总用电量'], color='orange')
+# plt.grid(axis='y')
+plt.xticks(rotation=-30)  # 斜着展示，角度从逆时针开始算。
+plt.title('每日用电量统计', fontsize=25, ha="center")
+plt.xlabel('时间（日）')
+plt.ylabel('用电量（KWh）')
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.show()
+path = './/plot_folder_user//'
+plt.savefig(path + '每日用电量统计')
+plt.cla()
+plt.close("all")
